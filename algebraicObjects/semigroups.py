30aade2b6380dc960a1f0f2b84c70721add4128e
Luispapienik <lpapiernik24@gmail.com>
2019-03-16 23:45:41 -0500

    se agrego funcionalidad a la inicializacion con tablas en formato de diccionario, falta arreglar un test. Se puede optimizar la funcion que chequea si una tabla en formato diccionario es correcta

--- algebraicObjects/semigroups.py
+++ algebraicObjects/semigroups.py
@@ -16,8 +16,14 @@ class ElementsWithoutOperationError(MAthError):
         self.args = ('elements without operations',)
 
 
+class MultivaluedOperation(MAthError):
+    def __init__(self):
+        self.args = ('multivalued operation',)
+
+
 class SemiGroup(object):
-    """Esta clase representa un semigrupo, un semigrupo es un conjunto no vacio
+    """
+       Esta clase representa un semigrupo, un semigrupo es un conjunto no vacio
        S con una operacion binaria multiplicacion (*): S x S -> S
 
        Atributos
@@ -26,10 +32,11 @@ class SemiGroup(object):
        elements: conjunto con los elementos del semigrupo
        multiplicacionTable: diccionario que especifica como realizar la
             operacion con los elementos del semigrupo
-       """
+    """
 
     def __init__(self, name, elements, multiplicationTable, format=MATRIX):
-        """name: nombe del semigrupo
+        """
+           name: nombe del semigrupo
            elements: iterable con los elementos del semigrupo
            multiplicationTable: dict o list, es la tabla de multiplicacion.
                 Si se pasa un diccionario, la clave es una tupla de elementos
@@ -41,46 +48,165 @@ class SemiGroup(object):
                 componente de la columan 1 y b[j] es la j-esima componente de
                 la fila j
            format: MATRIX=0 o DICT=1, indica en que formato se ingreso la tabla
-                de multiplicacion"""
-
+                de multiplicacion
+        """
+        elements = set(elements)
         self.name = name
 
         if format == MATRIX:
             self._check_table(elements, multiplicationTable)
 
-            self.elements = set(elements)
+            self.elements = elements
+            # self.multiplicationTable = multiplicationTable
+            self.multiplicationTable = self._table2map(multiplicationTable)
+
+        if format == DICT:
+            self._check_map(elements, multiplicationTable)
+
+            self.elements = elements
             self.multiplicationTable = multiplicationTable
 
-    def _check_table(self, elements, table):
-        """Esta funcion retorna True si table es una table de multiplicacion
+    def _check_map(self, elements, table):
+        """
+           Esta funcion chequea si table es una table de multiplicacion
            valida. Una tabla es validad si para todo par de elementos de
            elements esta definida la operacion, ademas no puede tener
            operaciones fuera del dominio
 
-           parametros
+           Parametros
+           ----------
+           elements: iterable con los elementos del semigrupo
+           table: dict. Tabla de multiplicacion a testear
+
+           Return
+           ------
+           out: True en caso de que la tabla sea correcta, se lanza un error
+                en caso contrario
+        """
+        # la operacion esta bien definida si para todo par de elementos esta
+        # definida, ademas la operacion debe ser cerrada
+
+        # la operacion es cerrada
+        for i in table.values():
+            if not (i in elements):
+                raise OperationOutOfDomainError()
+
+        # el diccionario debe tener n^2 elementos, con n el numero de elementos
+        # del semigrupo, las siguientes lineas no son estrictamentes necesarias
+        # pero optiizan el mejor de los casos cuando hay una excepcion
+        # -----------------------------------------
+        # numberOfElements = len(elements)
+        # if (numberOfElements ** 2) < len(table):
+        #     raise ElementsWithoutOperationError()
+        # if (numberOfElements ** 2) > len(table):
+        #     raise MultivaluedOperation()
+        # -----------------------------------------
+
+        # cada elemento a_i debe estar operado a izquierda con los elementos
+        # a_j, es decir, a_i * a_j debe tener un valor asociado
+
+        # para cada elemento se va a crear un conjunto de elementos de la forma
+        # (a_j, value) el cual representa a_i * a_j = value
+        counter = {}
+        numberOfElements = len(elements)
+        for (element, operating), value in table.items():
+            if not counter.get(element, 0):
+                counter[element] = set([(operating, value)])
+            else:
+                counter[element].add((operating, value))
+
+        # como cada elemento esta operado con los demas elementos del conjunto
+        # incluyendose, entonces los conjuntos formados anteriormente tienen
+        # exactamente numberOfElements elementos
+        for element in elements:
+            if not counter.get(element, 0):
+                raise ElementsWithoutOperationError()
+            # si es menor es porque hubieron parejas no relacionadas
+            if len(counter[element]) < numberOfElements:
+                raise ElementsWithoutOperationError()
+            # si es mayor es porque un numero se relaciono mas de dos veces con
+            # algun otro elemento
+            if len(counter[element]) > numberOfElements:
+                raise MultivaluedOperation()
+
+    def __raises(self, subset, elements):
+        """
+        Esta funcion es un ayudante para _check_table, no debe ser usada en
+        ninguna parte del codigo distinta a la funcion _check_table
+        """
+
+        for element in elements:
+            count = subset.count(element)
+            if count == 0:
+                raise ElementsWithoutOperationError()
+            if count >= 2:
+                raise MultivaluedOperation()
+
+    def _check_table(self, elements, table):
+        """
+           Esta funcion chequea si table es una table de multiplicacion
+           valida. Una tabla es validad si para todo par de elementos de
+           elements esta definida la operacion, ademas no puede tener
+           operaciones fuera del dominio
+
+           Parametros
            ----------
            elements: iterable con los elementos del semigrupo
            table: list. Tabla de multiplicacion a testear
-           """
-        # mirar que la operacion esta definida para todo par de elementos
-        subset = set(table[0])  # elementos en la primera fila
-        for element in elements:
-            if not (element in subset):
-                raise ElementsWithoutOperationError()
 
-        # elementos en la primera columna
-        subset = set([table[i][0] for i in range(len(table))])
-        for element in elements:
-            if not (element in subset):
-                raise ElementsWithoutOperationError()
+           Return
+           ------
+           out: True en caso de que la tabla sea correcta, se lanza un error
+                en caso contrario
+        """
+        # la operacion esta bien definida si para todo par de elementos esta
+        # definida, esto esta asegurado si cada elemento aparece solo
+        # una vez en la primera fila y solo una vez en la primera columna,
+        # ademas la operacion debe ser cerrada
 
-        # la tabla de multiplicacion esta dentro del dominio
+        # los elementos aparecen una vez en la fila 0
+        subset = table[0][1:]
+        self.__raises(subset, elements)
+
+        # los elementos aparecen una vez en la columna 0
+        subset = [table[i][0] for i in range(1, len(table))]
+        self.__raises(subset, elements)
+
+        # la operacion es cerrada
         for i in range(1, len(table)):
             for j in range(1, len(table[0])):
                 if not(table[i][j] in elements):
                     raise OperationOutOfDomainError()
 
         return True
+
+    def _table2map(self, table):
+        """
+           Esta funcion convierte la tabla de multiplicacion en formato MATRIX
+           a tabla de multiplicacion en formato DICT
+
+           Parametros
+           ----------
+           table: list. tabla de multiplicacion en formato MATRIX
+
+           Return
+           ------
+           out: dict. tabla de multiplicaion en formato DICT
+        """
+
+        # se debe recorrer cada elemento a_i de la primera columna, y para cada
+        # elemento recorrer los elementos b_i de la primera fila, formado el
+        # par (a_i, b_i) que es la clave del diccionario
+
+        tableDictFormart = {}
+        numberOfElements = len(table[0])
+
+        for i in range(1, numberOfElements):  # ciclo sobre la columna 0
+            for j in range(1, numberOfElements):  # ciclo sobre la fila 0
+                # se crea la entrada con la operacion en el diccionario
+                tableDictFormart[(table[i][0], table[0][j])] = table[i][j]
+
+        return tableDictFormart
 
 
 def main():
--- test/test_semigroups.py
+++ test/test_semigroups.py
@@ -1,20 +1,29 @@
 from algebraicObjects.semigroups import SemiGroup
 from algebraicObjects.semigroups import OperationOutOfDomainError
 from algebraicObjects.semigroups import ElementsWithoutOperationError
+from algebraicObjects.semigroups import MultivaluedOperation
+from algebraicObjects.semigroups import DICT
+
 
 import unittest
 
 
 class Test_SemiGroup(unittest.TestCase):
-    def test_check_table(self):
+    def test_initializationWithMatrix(self):
         # tabla correcta
         semigroup = SemiGroup('Z3', [0, 1, 2], [[0, 0, 1, 2],
                                                 [0, 0, 1, 2],
                                                 [1, 1, 2, 0],
                                                 [2, 2, 0, 1]])
 
-        self.assertTrue(semigroup._check_table(semigroup.elements,
-                                               semigroup.multiplicationTable))
+        self.assertIsInstance(semigroup, SemiGroup)
+
+        semigroup = SemiGroup('Z3', '012', [['0', '0', '1', '2'],
+                                            ['0', '0', '1', '2'],
+                                            ['1', '1', '2', '0'],
+                                            ['2', '2', '0', '1']])
+
+        self.assertIsInstance(semigroup, SemiGroup)
 
         # tabla con operaciones fuera del conjunto de elementos
         self.assertRaises(OperationOutOfDomainError, SemiGroup, 'Z3',
@@ -25,10 +34,96 @@ class Test_SemiGroup(unittest.TestCase):
 
         # tabla sin especificacion de operacion para pares de elementos
         self.assertRaises(ElementsWithoutOperationError, SemiGroup, 'Z3',
-                          [0, 1, 2], [[0, 0, 1, 0],
+                          [0, 1, 2], [[0, 0, 1],
+                                      [0, 0, 1],
+                                      [1, 1, 2],
+                                      [2, 2, 0]])
+
+        self.assertRaises(ElementsWithoutOperationError, SemiGroup, 'Z3',
+                          [0, 1, 2], [[0, 0, 1, 2],
                                       [0, 0, 1, 0],
-                                      [1, 1, 2, 1],
                                       [2, 2, 0, 2]])
+
+        # tablas con operacion multivaluada para pares de elementos
+        self.assertRaises(MultivaluedOperation, SemiGroup, 'Z3',
+                          [0, 1, 2], [[0, 0, 1, 2, 2],
+                                      [0, 0, 1, 2, 1],
+                                      [1, 1, 2, 0, 2],
+                                      [2, 2, 0, 1, 0]])
+
+        self.assertRaises(MultivaluedOperation, SemiGroup, 'Z3',
+                          [0, 1, 2], [[0, 0, 1, 2],
+                                      [0, 0, 1, 2],
+                                      [1, 1, 2, 0],
+                                      [2, 2, 0, 1],
+                                      [1, 0, 1, 2]])
+
+        self.assertRaises(MultivaluedOperation, SemiGroup, 'Z3',
+                          [0, 1, 2], [[0, 0, 1, 2, 2],
+                                      [0, 0, 1, 2, 1],
+                                      [1, 1, 2, 0, 2],
+                                      [2, 2, 0, 1, 0],
+                                      [1, 0, 1, 2, 2]])
+
+    def test_table2map(self):
+        tableMatrix = [[0, 0, 1, 2],
+                       [0, 0, 1, 2],
+                       [1, 1, 2, 0],
+                       [2, 2, 0, 1]]
+
+        tableDict = {(0, 0): 0, (0, 1): 1, (0, 2): 2, (1, 0): 1, (1, 1): 2,
+                     (1, 2): 0, (2, 0): 2, (2, 1): 0, (2, 2): 1}
+
+        semigroup = SemiGroup('Z3', [0, 1, 2], tableMatrix)
+
+        self.assertEqual(semigroup._table2map(tableMatrix), tableDict)
+
+    # @unittest.skip('test en construccion')
+    def test_initializationWithDict(self):
+        # tabla correcta
+        semigroup = SemiGroup('Z3', [0, 1, 2],
+                              {(0, 0): 0, (0, 1): 1, (0, 2): 2,
+                               (1, 0): 1, (1, 1): 2, (1, 2): 0,
+                               (2, 0): 2, (2, 1): 0, (2, 2): 1}, DICT)
+
+        self.assertIsInstance(semigroup, SemiGroup)
+
+        semigroup = SemiGroup('Z3', '012',
+                              {('0', '0'): '0', ('0', '1'): '1',
+                               ('0', '2'): '2', ('1', '0'): '1',
+                               ('1', '1'): '2', ('1', '2'): '0',
+                               ('2', '0'): '2', ('2', '1'): '0',
+                               ('2', '2'): '1'}, DICT)
+
+        self.assertIsInstance(semigroup, SemiGroup)
+
+        # tabla con operaciones fuera del conjunto de elementos
+        self.assertRaises(OperationOutOfDomainError, SemiGroup, 'Z3',
+                          [0, 1, 2], {(0, 0): 0, (0, 1): 1, (0, 2): 2,
+                                      (1, 0): 1, (1, 1): 2, (1, 2): 0,
+                                      (2, 0): 2, (2, 1): 0, (2, 2): 4}, DICT)
+
+        # tabla sin especificacion de operacion para pares de elementos
+        self.assertRaises(ElementsWithoutOperationError, SemiGroup, 'Z3',
+                          [0, 1, 2], {(0, 0): 0, (0, 1): 1,
+                                      (1, 0): 1, (1, 1): 2,
+                                      (2, 0): 2, (2, 1): 0}, DICT)
+
+        self.assertRaises(ElementsWithoutOperationError, SemiGroup, 'Z3',
+                          [0, 1, 2], {(0, 0): 0, (0, 1): 1, (0, 2): 2,
+                                      (2, 0): 2, (2, 1): 0, (2, 2): 1}, DICT)
+
+        self.assertRaises(ElementsWithoutOperationError, SemiGroup, 'Z3',
+                          [0, 1, 2], {(0, 0): 0, (0, 1): 1, (0, 2): 2,
+                                      (1, 0): 1, (1, 2): 0,
+                                      (2, 0): 2, (2, 1): 0, (2, 2): 1}, DICT)
+
+        # tablas con operacion multivaluada para pares de elementos
+        # self.assertRaises(MultivaluedOperation, SemiGroup, 'Z3',
+        #                   [0, 1, 2], {(0, 0): 0, (0, 1): 1, (0, 2): 2,
+        #                               (1, 0): 1, (1, 1): 2, (1, 2): 0,
+        #                               (2, 0): 2, (2, 1): 0, (2, 2): 1,
+        #                               (0, 2): 1}, DICT)
 
 
 def main():
